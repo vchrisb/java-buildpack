8b85022f9ffb001359588e277aedeb099e047e99
vchrisb <vchrisb@users.noreply.github.com>
2020-11-25 18:54:18 +0100

    Update app_dynamics_agent.rb

--- lib/java_buildpack/framework/app_dynamics_agent.rb
+++ lib/java_buildpack/framework/app_dynamics_agent.rb
@@ -200,12 +200,11 @@ module JavaBuildpack
       def override_default_config_local
         return unless @application.environment['APPD_CONF_DIR']
 
-        app_conf_dir = @application.root + @application.environment['APPD_CONF_DIR']
+        app_conf_dir = @application.environment['APPD_CONF_DIR']
         @logger.info { "Copy override configuration files from #{app_conf_dir}" }
-        app_conf_dir = @application.root + @application.environment['APPD_CONF_DIR'] + 'sdfs'
 
         CONFIG_FILES.each do |conf_file|
-          conf_file_path = conf_file
+          conf_file_path = @application.root + app_conf_dir + app_conf_dir
           @logger.info { "Copying #{app_conf_dir}" }
 
           next unless File.file?(conf_file_path)
